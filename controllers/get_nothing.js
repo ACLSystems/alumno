79c45f6798e7db3cb382f8b9b8cae416b66d2426
Arturo Castro <arturocastro@aclsystems.mx>
2018-01-25 14:40:32 -0600

    Agregando la version en el saludo inicial

--- controllers/get_nothing.js
+++ controllers/get_nothing.js
@@ -1,6 +1,12 @@
+const version = require('../shared/version');
+
 module.exports = {
 	greeting(req, res) {
 		res.status(200);
-		res.json({ hi: 'greetings'});
+		res.json({
+			hi: 'greetings',
+			app: version.app,
+			version: version.version
+		});
 	}
 };
