2b0bdc06bc416dba08a5cff4857c198d823b9414
Arturo Castro <arturocastro@aclsystems.mx>
2018-04-20 17:35:03 -0500

    listRoster

--- controllers/group_controller.js
+++ controllers/group_controller.js
@@ -495,7 +495,9 @@ module.exports = {
 											}
 											var b = 0;
 											while (b<numBlocks && !found) {
+												console.log('blocks[b]: ' + blocks[b] ' g.block._id: ' + g.block._id);
 												if(blocks[b] + '' === g.block._id + '') {
+													console.log('--------> here <--------');
 													send_grade.section 	= blocks[b].section;
 													send_grade.number		= blocks[b].number;
 													found = true;
