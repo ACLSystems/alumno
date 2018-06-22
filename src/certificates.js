71a8cdd5eeaa87f419805eab66cef379fb10a4c4
Arturo Castro <arturocastro@aclsystems.mx>
2018-06-22 11:43:04 -0500

    Agregando middleware al certificado para relacionarlo con el roster. De esta manera ya no estamos buscando el número cuando tenemos el roster

--- src/certificates.js
+++ src/certificates.js
@@ -1,5 +1,6 @@
 const mongoose 	= require('mongoose');
 const auto 			= require('mongoose-sequence')(mongoose);
+const Roster 		= require('./roster');
 const Schema 		= mongoose.Schema;
 
 mongoose.plugin(schema => { schema.options.usePushEach = true; });
@@ -17,6 +18,29 @@ const CertificateSchema = new Schema ({
 });
 
 CertificateSchema.plugin(auto,{inc_field: 'number'});
-
+CertificateSchema.post('save', function(next) {
+	Roster.findById(this.roster)
+		.then((item) => {
+			if(item) {
+				if(!item.certificateNumber || item.certificateNumber === 0) {
+					item.certificateNumber = this.number;
+					item.save()
+						.then(() => {
+							next();
+						})
+						.catch((err) => {
+							console.log('certificate middleware Error: Saving roster: ' + this.roster + ' Error: ' + err); //eslint-disable-line
+						});
+				} else {
+					next();
+				}
+			} else {
+				next();
+			}
+		})
+		.catch((err) => {
+			console.log('certificate middleware Error: Finding roster: ' + this.roster + ' Error: ' + err); //eslint-disable-line
+		});
+});
 const Certificate = mongoose.model('certificates', CertificateSchema);
 module.exports = Certificate;
