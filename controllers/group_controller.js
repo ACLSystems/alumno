b23a82826552c51cfe7aeb8d9db9eb16ada22ff5
Arturo Castro <arturocastro@aclsystems.mx>
2019-06-17 19:46:33 -0500

    Actualizando un error de apuntador en create course

--- controllers/group_controller.js
+++ controllers/group_controller.js
@@ -145,7 +145,7 @@ module.exports = {
 										if(course.type === 'tutor') {
 											mailjet.sendMail(supportEmail, 'Administrador', 'Alerta: Se ha generado un grupo de tipo tutor. Favor de gestionar. +' + group.code,679640,portal,'Se ha generado un grupo de tipo tutor. Favor de gestionar. +' + group.code + ' ' + group.course.title);
 										} else {
-											mailjet.sendMail(supportEmail, 'Administrador', 'Aviso: Se ha generado un grupo. + ' + group.code,679640,portal,'Se ha generado un grupo. + ' + group.code + ' ' + group.course.title);
+											mailjet.sendMail(supportEmail, 'Administrador', 'Aviso: Se ha generado un grupo. + ' + group.code,679640,portal,'Se ha generado un grupo. + ' + group.code + ' ' + course.title);
 										}
 									})
 									.catch((err) => {
